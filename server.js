18c66c98229e2074350e2c6a0aa75e77d19bb276
Hoang Tri <hoangsidrpr@gmail.com>
2022-11-11 09:28:30 +0700

    added new features

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine','ejs')
+
 app.get('/',(req,res)=> {
     console.log("here")
-    res.download("server.js")
-    res.json({message: "Error"})
-    //res.send("Hi")
+    res.render('index',{text: "wou"})
 })
+
+const userRouter=require('./routes/users')
+
+app.use('/users',userRouter)
 
 app.listen(3000)
